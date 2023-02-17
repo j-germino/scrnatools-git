101744848f87d89f69d73e198351edb8b949ea7c
joe <joe.germino98@gmail.com>
2023-02-17 17:23:00 +0000

    change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scrnatools",
-    version="0.3.2",
+    version="0.3.3",
     author="Joe Germino",
     author_email="joe.germino@ucsf.edu",
     description="Tools for single cell RNA sequencing pipelines",
