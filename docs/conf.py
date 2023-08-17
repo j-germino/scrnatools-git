5f80eec2dbabc0f591158444428b5e9b5ba5801b
joe <joe.germino98@gmail.com>
2023-08-17 22:03:29 +0000

    test rtd

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'scrnatools'
+copyright = '2023'
+author = 'Joe Germino'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.4'
+version = '0.4.0'
 
 # -- General configuration
 
